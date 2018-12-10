7b62c36e7fe829456fb4056c9491a8eb463812bc
sparud <jan@sparud.net>
2018-12-10 17:58:00 +0100

    2018, day 9

--- src/aoc2018.scala
+++ src/aoc2018.scala
@@ -42,8 +42,226 @@ object aoc2018 {
     val part2 = triangulate(input).flatMap(p => common(p._1, p._2)).head
   }
 
+  object day3 {
+    val input = Source.fromFile("data2018/3").getLines().toList
+
+    val pattern = """#(\d+) @ (\d+),(\d+): (\d+)x(\d+)""".r
+
+    val claims = input flatMap {
+      case pattern(id, x0, y0, w, h) =>
+        (x0.toInt until x0.toInt+w.toInt).flatMap(x => (y0.toInt until y0.toInt+h.toInt).map(y => (id, (x, y))))
+    }
+
+    val fabric = claims.foldLeft(Map[(Int, Int), Int]().withDefaultValue(0)){
+      case (map, (_, p)) => map + (p -> (map(p)+1))
+    }
+
+    val part1 = fabric.values.count(_ > 1)
+
+    val fabricIds = claims.foldLeft(Map[(Int, Int), List[String]]().withDefaultValue(Nil)){
+      case (map, (id, p)) => map + (p -> (id :: map(p)))
+    }
+
+    val fabricBuddies = fabricIds.values.foldLeft(Map[String, Set[String]]().withDefaultValue(Set.empty)) {
+      case (map, ids) => map ++ ids.map(id => id -> (map(id) ++ ids))
+    }
+
+    val part2 = fabricBuddies.values.find(_.size == 1).get.head
+  }
+
+  object day4 {
+    val input = Source.fromFile("data2018/4").getLines().toList
+
+    val p = """\[[^ ]+ \d+:(\d+)\] ([^ ]+) ([^ ]+).*""".r
+    val sleep = input.sorted.foldLeft((None: Option[Int], None: Option[Int], Map[Int, Map[Int, Int]]())) {
+      case ((who, start, state), p(time, a, b)) => a match {
+        case "Guard" => (Some(b.substring(1).toInt), None, state)
+        case "falls" => (who, Some(time.toInt), state)
+        case "wakes" =>
+          val whoMap = state.getOrElse(who.get, Map[Int, Int]().withDefaultValue(0))
+          (who, start, state + (who.get ->  (whoMap ++ (start.get until time.toInt).map(m => m -> (whoMap(m)+1)))))
+      }
+    }._3
+
+    val (sleeper, sleepTimes) = sleep.maxBy(_._2.values.sum)
+    val part1 = sleeper * sleepTimes.maxBy(_._2)._1
+
+    val (sleeper2, sleepTimes2) = sleep.maxBy(_._2.toList.maxBy(_._2)._2)
+    val part2 = sleeper2 * sleepTimes2.maxBy(_._2)._1
+  }
+
+  object day5 {
+    val input = Source.fromFile("data2018/5").mkString
+
+    def react(s: String) = s.foldLeft(List[Char]()) {
+      case (Nil, c) => List(c)
+      case (c1 :: rest, c2) if (c1 ^ c2) == 32 => rest
+      case (rest, c) => c :: rest
+    }.length
+
+    val part1 = react(input)
+
+    val part2 = input
+      .map(_.toUpper)
+      .toSet
+      .map((c: Char) => react(input.replace(c+"", "").replace(c.toLower+"", "")))
+      .min
+  }
+
+  object day6 {
+    val input = Source.fromFile("data2018/6").getLines.toList
+
+    case class Coord(x: Int, y: Int) {
+      def distance(otherX: Int, otherY: Int) = Math.abs(otherX - x) + Math.abs(otherY-y)
+    }
+    case object Coord {
+      def apply(s: String): Coord = s.split(", ") match {
+        case Array(x, y) => Coord(x.toInt, y.toInt)
+      }
+    }
+
+    val coordinates = input.map(Coord.apply)
+    val topLeft = Coord(coordinates.map(_.x).min, coordinates.map(_.y).min)
+    val bottomRight = Coord(coordinates.map(_.x).max, coordinates.map(_.y).max)
+
+    val grid = (topLeft.x to bottomRight.x).map(x =>
+      (topLeft.y to bottomRight.y).map { y =>
+        val (dist, which) = coordinates.map(_.distance(x, y)).zipWithIndex.groupBy(_._1).toList.minBy(_._1)
+        if (which.size > 1) -1 else which.head._2
+      })
+
+    val infinite = (grid.head ++ grid.last ++ grid.map(_.head) ++ grid.map(_.last)).toSet
+
+    val part1 = grid
+      .flatten
+      .groupBy(identity)
+      .mapValues(_.size)
+      .toSeq
+      .filter(p => !infinite.contains(p._1))
+      .maxBy(_._2)
+      ._2
+
+
+    val grid2 = (topLeft.x to bottomRight.x).flatMap(x =>
+      (topLeft.y to bottomRight.y).map { y =>
+        coordinates.map(_.distance(x, y)).sum < 10000
+      }).count(identity)
+
+    val part2 = grid2
+  }
+
+  object day7 {
+    val input = Source.fromFile("data2018/7").getLines.toList
+    val pattern = """Step (.) must be finished before step (.) can begin.""".r
+
+    val rules = input map {
+      case pattern(from, to) => (from, to)
+    }
+
+    val dependencies = rules.groupBy(_._2).mapValues(_.map(_._1).mkString).toList.map(_.swap)
+
+    val startState = dependencies ++ (dependencies.flatMap(_._1).toSet -- dependencies.map(_._2(0))).map("" -> _.toString)
+
+    val part1 = startState.foldLeft((List[String](), startState.sorted)) {
+      case ((builder, ("", step)::rest), _) =>
+        (step :: builder, rest.map(p => (p._1.replace(step, ""), p._2)).sorted)
+    }._1.reverse.mkString
+
+    val NWORKERS = 5
+    val EXTRA = 60
+
+    def iterate(state: List[(String, String)], time: Int = 0, workers: List[(String, Int)] = Nil): Int = {
+      if (state.isEmpty && workers.isEmpty)
+        time
+      else {
+        val (ready, rest) = state.partition(_._1.isEmpty)
+        val newWorkers = workers ++ ready.take(NWORKERS - workers.size).map(_._2).map(s => (s, s(0).toInt - 64 + EXTRA))
+        val sortedWorkers = newWorkers.groupBy(_._2).mapValues(_.map(_._1).mkString).toList.sorted
+        val (delta, steps, workingWorkers) = sortedWorkers.headOption.map {
+          case (delta, steps) => (delta, steps.mkString, sortedWorkers.tail.flatMap(p => p._2.map(c => (c.toString, p._1))))
+        }.getOrElse((0, "", Nil))
+        iterate(
+          ready.drop(NWORKERS - workers.size) ++ rest.map(p => ((p._1.toSet -- steps).mkString, p._2)),
+          time + delta,
+          workingWorkers.map(p => (p._1, p._2-delta)))
+      }
+    }
+
+    val part2 = iterate(startState)
+  }
+
+  object day8 {
+    val input = Source.fromFile("data2018/8").mkString.split(" ").map(_.toInt)
+
+    case class Node(children: Seq[Node], metadata: Seq[Int]) {
+      def sumMetadata(): Int = metadata.sum + children.map(_.sumMetadata()).sum
+      def value(): Int = if (children.isEmpty)
+          sumMetadata()
+        else
+          metadata.map(ix => if (ix > 0 && ix <= children.size) children(ix-1).value() else 0).sum
+    }
+
+    object Node {
+      def apply(numbers: Iterator[Int]): Node = {
+        val nChildren = numbers.next()
+        val nMetadata = numbers.next()
+        Node((1 to nChildren).map(_ => Node.apply(numbers)), (1 to nMetadata).map(_ => numbers.next()))
+      }
+    }
+
+    val part1 = Node.apply(input.toIterator).sumMetadata()
+    val part2 = Node.apply(input.toIterator).value()
+  }
+
+  object day9 {
+    case class Entry[A](value: A, var prev: Entry[A] = null, var next: Entry[A] = null) {
+      if (prev == null)
+        prev = this
+      if (next == null)
+        next = this
+
+      def insert(value: A): Entry[A] = {
+        val entry = Entry(value, prev, this)
+        prev.next = entry
+        this.prev = entry
+        entry
+      }
+      def remove(): (A, Entry[A]) = {
+        prev.next = next
+        next.prev = prev
+        (value, next)
+      }
+      def moveCounterClockwise(steps: Int): Entry[A] = (1 to steps).foldLeft(this){ case (entry, _) => entry.prev }
+      def moveClockwise(steps: Int): Entry[A] = (1 to steps).foldLeft(this){ case (entry, _) => entry.next }
+    }
+
+    def play(nPlayers: Int, endMarble: Int) = {
+      val players = new Array[Long](nPlayers)
+
+      def iterate(round: Int = 1, circle: Entry[Int] = Entry(0)): Long = {
+        if (round > endMarble) {
+          players.max
+        } else if (round % 23 == 0) {
+          val entry = circle.moveCounterClockwise(7)
+          val (removed, nextEntry) = entry.remove()
+          players(round % nPlayers) += removed + round
+          iterate(round + 1, nextEntry)
+        } else {
+          iterate(round + 1, circle.moveClockwise(2).insert(round))
+        }
+      }
+
+      iterate()
+    }
+
+    val part1 = play(410, 72059)
+
+    val part2 = play(410, 72059*100)
+
+  }
+
   def main(args: Array[String]) {
-    println(day2.part1)
-    println(day2.part2)
+    println(day9.part1)
+    println(day9.part2)
   }
 }
